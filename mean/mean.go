9778a73126bf8666e6fa468813318f92de803f57
khahussein <kherld.hussein@gmail.com>
2024-09-25 11:35:12 +0300

    Feat(mean): Implement mean of sample data

---
+++ mean/mean.go
@@ -0,0 +1,13 @@
+package mean
+
+// Mean is a single number or value that best represents a set of data.
+func Mean(x []float64) float64 {
+	if len(x) == 0 {
+		return 0
+	}
+	var total float64
+	for _, x1 := range x {
+		total += x1
+	}
+	return total / float64(len(x))
+}
