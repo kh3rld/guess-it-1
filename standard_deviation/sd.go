1c791cb21645298919f984aedaf9e79f62a6de7c
khahussein <kherld.hussein@gmail.com>
2024-09-25 12:14:17 +0300

    Refactor(sd): Add mean and refactor Standard deviation to take only float

--- standard_deviation/sd.go
+++ standard_deviation/sd.go
@@ -3,9 +3,11 @@ package standarddeviation
 import (
 	"math"
 
+	"github.com/kh3rld/guess-it-1/mean"
 	"github.com/kh3rld/guess-it-1/variance"
 )
 
-func StandardD(x []float64, m float64) float64 {
+func StandardD(x []float64) float64 {
+	m := mean.Mean(x)
 	return math.Sqrt(variance.Variance(x, m))
 }
