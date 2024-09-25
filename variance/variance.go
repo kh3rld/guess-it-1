8cb0b1ded22cf91af668f0894937c4a64e3ad4a3
khahussein <kherld.hussein@gmail.com>
2024-09-25 11:35:59 +0300

    Feat(variance): Implement Variance of sample data from mean

---
+++ variance/variance.go
@@ -0,0 +1,14 @@
+package variance
+
+// Variance is the expected value of the squared deviation from the mean of a random variable.
+func Variance(x []float64, m float64) float64 {
+	t := 0.0
+	if len(x) == 0 {
+		return 0
+	}
+	for _, x1 := range x {
+		y := x1 - m
+		t += y * y
+	}
+	return t / float64(len(x))
+}
