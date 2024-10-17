4dc633cf057b81fbe35779a5af92d1262aec188c
khahussein <kherld.hussein@gmail.com>
2024-10-17 08:54:18 +0300

    Feat(mean): Update mean function that calculates the mean of a slice of float64 numbers

--- mean/mean.go
+++ mean/mean.go
@@ -1,13 +1,13 @@
 package mean
 
-// Mean is a single number or value that best represents a set of data.
-func Mean(x []float64) float64 {
-	if len(x) == 0 {
+// Calculate the mean of a slice of float64 numbers
+func Mean(data []float64) float64 {
+	if len(data) == 0 {
 		return 0
 	}
 	var total float64
-	for _, x1 := range x {
-		total += x1
+	for _, value := range data {
+		total += value
 	}
-	return total / float64(len(x))
+	return total / float64(len(data))
 }
