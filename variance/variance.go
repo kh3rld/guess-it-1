0a3d58bae81c54a3a6f947c23f73c91f0111a805
kh3rld <kherld.hussein@gmail.com>
2024-10-19 09:37:33 +0300

    Refactor(variance): Refactor Variance function to use mean calculation
    - Removed the second parameter (mean) and replaced it with a call to the new mean.Mean function to compute the mean from the input slice.
    - Simplified the computation of squared deviations by appending them to a new slice.
    - Improved readability by using descriptive variable names and separating the mean calculation from the variance calculation.

--- variance/variance.go
+++ variance/variance.go
@@ -1,14 +1,14 @@
 package variance
 
+import "github.com/kh3rld/guess-it-1/mean"
+
 // Variance is the expected value of the squared deviation from the mean of a random variable.
-func Variance(x []float64, m float64) float64 {
-	t := 0.0
-	if len(x) == 0 {
-		return 0
-	}
+func Variance(x []float64) float64 {
+	m := mean.Mean(x)
+	r := []float64{}
 	for _, x1 := range x {
 		y := x1 - m
-		t += y * y
+		r = append(r, y*y)
 	}
-	return t / float64(len(x))
+	return mean.Mean(r)
 }
